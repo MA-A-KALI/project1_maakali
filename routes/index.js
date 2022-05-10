a4fa6d4af42c077dba9aefcfca72cf40bf64a82f
shingo-madre <rakazein.akbar@gmail.com>
2022-05-10 19:05:20 +0700

    new route on home

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET pages function. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('home');
 });
 
 router.get('/about', function(req, res) {
-  res.render('home')
+  res.json({message : 'this is about page'})
+});
+
+router.get('/services', (req, res) => {
+  res.json({message: 'this is service page'});
+});
+
+router.get('/hero_register', (req, res) => {
+  res.json({message: 'this is hero register page'});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const consumerController = require('../controllers/consumerController');
 
-
-/* GET users listing. */
-router.get('/', function(req, res, /*next*/) {
+/* API routes */
+router.get('/login', function(req, res, /*next*/) {
   res.send('respond with a resource');
 });
+
+/* consumer login and register */
+router.get('/register', consumerController.consumer_create_get);
+
+router.post('/register', consumerController.consumer_create_post);
+
+
 
 module.exports = router;
